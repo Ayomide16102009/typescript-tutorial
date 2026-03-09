f8a6399820718eb6fa80a8d87a31f60a40882983
Teejay2443 <tijaniabdulafeez05@gmail.com>
2026-03-09 13:16:03 +0100

    - Add Todo model with id, todo, and isDone fields - Implement TodoList and SingleTodo components - Wire up handleAdd in App to manage todos state - Connect InputFeild with form submission and ref-based blur
    - Add styles for todos, todos__single, and icon elements

--- src/component/InputFeild.tsx
+++ src/component/InputFeild.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './styles.css';
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd:(e: React.FormEvent)=>void;
 }
 
-const InputFeild = ({ todo, setTodo }: Props) => {
+const InputFeild = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  
   return (
-    <form className='input'>
-      <input type="text" placeholder='Enter a task' className='input_box'/>
+    <form className='input' onSubmit={(e)=>{
+      handleAdd(e)
+      inputRef.current?.blur();
+    }}>
+      <input
+        ref={inputRef}
+        type="input"
+        value={todo}
+        onChange={(e)=>setTodo(e.target.value)}
+        placeholder='Enter a task'
+        className='input_box'
+      />
       <button className='input_submit' type='submit'>
         Go
       </button>
